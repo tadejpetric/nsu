bd61e035cdd65c8119d179e843d53e258d0c049f
Tadej <tadej.petric1@gmail.com>
2023-05-20 19:06:47 +0200

    broken version for parsing, saving before refactor

---
+++ dn4/generation.py
@@ -0,0 +1,65 @@
+import ProGED as pg
+import numpy as np
+
+def one_generation(eq, speed):
+
+    gramatika = eq.gen_grammar()
+    generator = pg.GeneratorGrammar(gramatika)
+    ED = pg.EqDisco(data=eq.data, 
+                    lhs_vars=eq.lhs,
+                    rhs_vars=eq.rhs,
+                    generator=generator,
+                    sample_size=1000)
+    ED.generate_models()
+    ED.fit_models()
+    print(ED.get_results())
+
+    E = np.array(eq.E)
+    F = np.array(eq.F)
+    T = np.array(eq.T)
+    V = np.array(eq.V)
+
+    for p in ED.get_results(1):
+        equation = str(p.expr)[1:-1]
+        cr, cv = eq.parse(equation)
+        speed = (np.tanh(-np.log2(p.estimated["fun"])/4)+1)/2
+        print(speed)
+        # "+":0, "-": 0, "F": 0,
+        total = cr["+"] + cr["-"] + cr["F"]
+        # desired is [cr["+"], cr["-"]] / total
+        E2 = np.array([cr["+"], cr["-"]])/total
+        total = cr["*"] + cr["/"] + cr["T"]
+        F2 = np.array([cr["*"], cr["/"]])/total
+        total = cr["V"] + cr["("] + cr["V"]
+        T2 = np.array([cr["V"], cr["("]])/total
+        total = sum(cv.values())
+        del cv[f"x{len(V)}"]
+        V2 = np.array(list(cv.values()))/total
+
+        E = E*(1-speed) + E2*speed
+        F = F*(1-speed) + F2*speed
+        T = T*(1-speed) + T2*speed
+        V = V*(1-speed) + V2*speed
+    
+    return (list(E),list(F),list(T),list(V))
+
+def evolve(eq, gens):
+    for i in range(gens):
+        E, F, T, V = one_generation(eq, 0.1)
+        print(E,F,T,V)
+        eq.E = E
+        eq.F = F
+        eq.T = T
+        eq.V = V
+    return eq
+
+if __name__ == "__main__":
+    from make_data import *
+
+    @pandise
+    def first(x1, x2, x3, x4, x5):
+        return x1 - 3*x2 - x3 -x5
+    
+    eq1 = problem(first)
+    eq1.make_data(500)
+    evolve(eq1, 50)
--- dn4/make_data.py
+++ dn4/make_data.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from functools import wraps
 from inspect import signature
+from nltk.tokenize import wordpunct_tokenize
 
 def variable(i):
     # doesn't work for like >25 variables
@@ -23,14 +24,48 @@ def makegraph(function, rows, lower, upper):
     np.random.seed(1)
     dataset = pd.DataFrame()
     for i in range(columns):
-        dataset[variable(i)] = np.random.default_rng(i).uniform(lower, upper, rows)
+        dataset[f"x{i}"] = np.random.default_rng(i).uniform(lower, upper, rows)
     
     dataset["y"] = function(dataset)
     return dataset
 
 def get_rhs(function):
     columns = len(signature(function).parameters)
-    return [str(variable(i)) for i in range(columns)]
+    return [f"x{i}" for i in range(columns)]
+
+def tokenize(s):
+    s = s.replace("+", "+ ")
+    tokens_pre = wordpunct_tokenize(s)
+    tokens2 = []
+    
+    prevpow = False
+    for tk in tokens_pre:
+        if prevpow:
+            tokens2.append("**"+tk)
+            prevpow = False
+        elif tk != "**":
+            tokens2.append(tk)
+        else:
+            prevpow = True
+
+    tokens = []
+    prevminus=False 
+    for tk in tokens2:
+        if prevminus:
+            prevminus = False
+            try:
+                int(tk[0])
+                tokens.append("-"+tk)
+            except ValueError:
+                tokens.append("-")
+                tokens.append(tk)
+        if tk == "-":
+            prevminus = True
+        else:
+            tokens.append(tk)
+    
+    return tokens
+
 
 class problem:
     def __init__(self, function):
@@ -64,18 +99,139 @@ class problem:
         return 2
     def xtoi(s):
         return int(s[1:]) - 1
+    
     def gen_grammar(self):
         s = ""
-        s += f"E -> E '+' F [{self.E[0]}] | E '-' F [{self.E[1]}] | F [{1-self.E[0]-self.E[1]}]\n"
-        s += f"F -> F '*' T [{self.F[0]}] | F '/' T [{self.F[1]}] | T [{1-self.F[0]-self.F[1]}]\n"
-        s += f"T -> V [{self.T[0]}] | '(' E ')' [{self.T[1]}] | 'sin(' E ')' [{1-self.T[0]-self.T[1]}]\n"
+        s += f"E -> E '+' F [{self.E[0]:0.16f}] | E '-' F [{self.E[1]:0.16f}] | F [{1-self.E[0]-self.E[1]:0.16f}]\n"
+        s += f"F -> F '*' T [{self.F[0]:0.16f}] | F '/' T [{self.F[1]:0.16f}] | T [{1-self.F[0]-self.F[1]:0.16f}]\n"
+        s += f"T -> V [{self.T[0]:0.16f}] | '(' E ')' [{self.T[1]:0.16f}] | 'sin(' E ')' [{1-self.T[0]-self.T[1]:0.16f}]\n"
         s += "V -> "
 
         total = 0
         # gramatika je od 0 do n-1
         for i, prob in enumerate(self.V):
             total += prob
-            s += f"'{variable(i)}' [{prob}] | "
+            s += f"'x{i}' [{prob:0.16f}] | "
         
-        s += f"'{variable(i+1)}' [{1-total}]"
+        s += f"'x{i+1}' [{1-total:0.16f}]"
         return s
+    
+    def parse(self, s : str):
+        # every parsing of E concludes with the third rule. If + was enacted once, F was then enacted in addition
+        tokens = tokenize(s)
+        count_rules = {"+":0, "-": 0, "F": 0, "*": 0, "/": 0, "T": 0, "V":0, "(":0, "sin":0}
+        count_vars = {f"x{i}":0 for i in range(len(self.V)+1)}
+        was_sin = False
+        no_E_op = True
+        no_F_op = True
+
+        
+        for i,token in enumerate(tokens):
+            if token[:2] == "**":
+                no_F_op = False
+                z = int(token[2:])-1
+                count_rules["*"] += z*2
+                count_rules["T"] += z
+                if tokens[i-1][0] == "x":
+                    count_vars[tokens[i-1]] += z
+                continue
+            if was_sin:
+                # if previous token was "sin", next token is "("
+                was_sin = False
+                continue
+            try:
+                # cases like 3(x1+x2) (not 3x1), artifact of parsing
+                z = int(token)-1
+                count_rules["+"] += z*2
+                count_rules["F"] += z
+                no_E_op = False
+                continue
+            except ValueError:
+                pass
+            if token[0]=="x":
+                # every xn came from a V
+                count_vars[token] += 1
+                count_rules["V"] += 1
+                count_rules["F"] += no_E_op
+                count_rules["T"] += no_F_op
+
+            elif "x" in token:
+                # cases like x2   +x1+x1 -> x2+  2x1 
+
+                left, right = token.split("x")
+                if left[0] == "-":
+                    # -2x1 = -x1-x1
+                    # technically -2x1 = x1-x1-x1-x1
+                    # but since x2 -2x1 = x2 -x1 -x1, we choose this route for simplicity
+                    left = int(left[1:])
+                    no_E_op = False
+                    count_rules["-"] += left*2
+                    count_rules["F"] += left
+                    count_vars["x"+right] += left
+                else:
+                    left = int(left)
+                    no_E_op = False
+                    count_rules["+"] += left*2
+                    count_rules["F"] += left
+                    count_vars["x"+right] += left
+            elif token == "+":
+                count_rules["+"] += 2
+                count_rules["F"] += 1
+                no_E_op = False
+            elif token == "-":
+                count_rules["-"] += 2
+                count_rules["F"] += 1
+                no_E_op = False 
+        
+
+            elif token == "sin":
+                count_rules["sin"] += 1
+                count_rules["F"] += no_E_op
+                count_rules["T"] += no_F_op
+                was_sin = True
+                no_F_op = True
+                no_E_op = True
+
+            elif token == "(":
+                count_rules["("] += 1
+                count_rules["F"] += no_E_op
+                count_rules["T"] += no_F_op
+                no_F_op = True
+                no_E_op = True
+            elif token == "*":
+                try:
+                    # 3*x1
+                    z = int(tokens[i-1])
+                    if i-1 > 0:
+                        if tokens[i-2] == "-":
+                            z *= -1
+                    if z > 0:
+                        count_rules["+"] += 2*z
+                        count_rules["F"] += z
+                        if tokens[i+1][0] == "x":
+                            count_vars[tokens[i+1]] += z
+                    else:
+                        z *= -1
+                        count_rules["-"] += 2*z
+                        count_rules["F"] += z
+                        if tokens[i+1][0] == "x":
+                            count_vars[tokens[i+1]] += z
+                except ValueError:
+                    count_rules["*"] += 2
+                    count_rules["T"] += 1
+                no_F_op = False
+            elif token == "/":
+                count_rules["/"] += 2
+                count_rules["T"] += 1
+                no_F_op = False
+
+        count_rules["F"] += no_E_op
+        count_rules["T"] += no_F_op
+        if tokens[0][0] == "-" and count_rules["+"] > 1:
+            count_rules["+"] -= 2
+        return (count_rules, count_vars)
+
+
+
+
+
